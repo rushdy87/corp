d813b44cd99ef3af8445fa809c328d43694c71c3
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-03-29 01:49:12 +0300

    Adding a Reusable Presentation Component

--- src/app/scale/page.tsx
+++ src/app/scale/page.tsx
@@ -1,3 +1,14 @@
+import Hero from '@/components/hero';
+import scaleImg from 'public/scale.jpg';
+
 export default function ScalePage() {
-  return <div>ScalePage</div>;
+  return (
+    <div>
+      <Hero
+        imgData={scaleImg}
+        imgAlt='steel factory'
+        title='Scale your app to infinty'
+      />
+    </div>
+  );
 }
